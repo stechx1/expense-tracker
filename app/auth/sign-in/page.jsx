797e39548ff2931b83abbdc2480cf05f6a102d2a
Sana Saeed <ss.uitech01@gmail.com>
2024-04-07 03:05:04 +0500

    added temporary auth

---
+++ app/auth/sign-in/page.jsx
@@ -0,0 +1,83 @@
+'use client';
+import { Button, Form, Input, Select } from 'antd';
+import { useState } from 'react';
+import Link from 'next/link';
+
+const SignIn = () => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className=' container mx-auto h-screen flex justify-center items-center'>
+      <div className='flex flex-col justify-center items-center space-y-10'>
+        <h1 className='text-4xl font-bold text-white'>Expense Tracker</h1>
+        <div className='rounded-2xl bg-white max-w-lg min-h-[515px] px-20 pt-12 pb-3'>
+          <h2 className='font-bold text-lg'>Sign Up to create an account</h2>
+          <Form
+            size='large'
+            name='basic'
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              paddingTop: '20px',
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={() => ''}
+            onFinishFailed={() => ''}
+            autoComplete='off'
+          >
+            <Form.Item
+              style={{
+                maxWidth: '100%',
+              }}
+              name='Email'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your email!',
+                },
+              ]}
+            >
+              <Input type='email' placeholder='Email' />
+            </Form.Item>
+
+            <Form.Item
+              name='password'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password placeholder='Password' />
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button loading={loading} type='primary' htmlType='submit'>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <Link
+            className='text-xs text-primary font-bold'
+            href={'/auth/sign-in'}
+          >
+            Already have an account? Log in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,24 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import theme from '../theme/themeConfig';
+import { ConfigProvider } from 'antd';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Expense Tracker",
-  description: "Created in Next JS",
+  title: 'Expense Tracker',
+  description: 'Created in Next JS',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <ConfigProvider theme={theme}>
+          <AntdRegistry>{children}</AntdRegistry>
+        </ConfigProvider>
+      </body>
     </html>
   );
 }
