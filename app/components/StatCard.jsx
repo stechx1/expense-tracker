715ff9c395ad4413ee99fc47591d2cdcdf9ea431
Sana Saeed <ss.uitech01@gmail.com>
2024-04-07 05:52:58 +0500

    dashboard cards dobe

---
+++ app/components/StatCard.jsx
@@ -0,0 +1,8 @@
+export const StatCard = ({ name, stat, textBased }) => {
+  return (
+    <div className="bg-[#F6F6F6] p-4 rounded-md ">
+      <h2 className="text-lg font-medium">{name}</h2>
+      {textBased? <p className="my-2 text-3xl font-bold">{stat}</p>: <p className="my-2 text-5xl font-bold">£ {stat}</p>}
+    </div>
+  );
+};
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
 import Image from 'next/image';
+import { StatCard } from './components/StatCard';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-3xl font-bold'>Dashboard</h1>
+    <main className='container mx-auto'>
+      <div className='grid grid-cols-4 my-10 gap-6'>
+        <StatCard name={'Overall Spent'} stat={200} />
+        <StatCard name={'This Year'} stat={120} />
+        <StatCard name={'This Month'} stat={45} />
+        <StatCard name={'This Week'} stat={45} />
+        <StatCard name={'Today'} stat={45} />
+        <StatCard textBased name={'Most Spent on'} stat={'Investment'} />
+        <StatCard name={'Most Spent day'} stat={'Saturday'} textBased />
+        <StatCard name={'Least Spent Day'} stat={'Friday'} textBased />
+      </div>
     </main>
   );
 }
