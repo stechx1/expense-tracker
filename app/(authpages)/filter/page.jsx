b1ee581cc13e7b7fcb4023101da85278614911da
saqib867 <saqibhussain9910@gmail.com>
2024-04-26 01:11:51 +0500

    stats

--- app/(authpages)/filter/page.jsx
+++ app/(authpages)/filter/page.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-'use client';
-import { DatePicker, InputNumber, Select } from 'antd';
-import { DataTable } from '../../components/DataTable';
-import { Button, Form, Input } from 'antd';
-import { useEffect, useState } from 'react';
-import { app, db } from '../../firebase/firebase';
-import { categories } from '../../data/categories';
+"use client";
+import { DatePicker, InputNumber, Select } from "antd";
+import { DataTable } from "../../components/DataTable";
+import { Button, Form, Input } from "antd";
+import { useEffect, useState } from "react";
+import { app, db } from "../../firebase/firebase";
+import { categories } from "../../data/categories";
 
-import { getAuth } from 'firebase/auth';
-import withAuth from '@/app/HOC/withAuth';
-import { DoughnutChart } from '@/app/components/DoughnutChart';
+import { getAuth } from "firebase/auth";
+import withAuth from "@/app/HOC/withAuth";
+import { DoughnutChart } from "@/app/components/DoughnutChart";
 const { RangePicker } = DatePicker;
 
 import {
@@ -22,7 +22,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
+import { collection, onSnapshot, orderBy, query, where } from "firebase/firestore";
 
 const Filter = () => {
   ChartJS.register(
@@ -35,134 +36,195 @@ const Filter = () => {
     Tooltip,
     Legend
   );
-  const [monthWiseData, setMonthWiseData] = useState();
+
   const [loading, setLoading] = useState(false);
   const auth = getAuth(app);
   const currentUser = auth.currentUser;
-  console.log('current user ', currentUser);
-  const currentDate = new Date();
-  console.log('month wise data ', monthWiseData);
+  const [filterData, setFilterData] = useState({
+    to: 0,
+    from: 0,
+    category: null,
+    startDate: null,
+    endDate: null,
+  });
 
-  const onStartDateChange = (date, dateString) => {
-    console.log(date, dateString);
+  const [result,setResult] = useState([])
+
+  const onDateChange = (e) => {
+    setFilterData({
+      ...filterData,
+      startDate: new Date(e[0]).toISOString(),
+      endDate: new Date(e[1]).toISOString(),
+    });
   };
 
-  const onEndDateChange = (date, dateString) => {
-    console.log(date, dateString);
+  const handleChange = (e, type) => {
+    setFilterData({ ...filterData, [type]: e });
   };
 
-  const handleSubmit = () => {
-    console.log('Submitted');
+  const handleSubmit = (values) => {
+    const category = values["category"];
+    const expenseEnd = values["expenseEnd"];
+    const expenseStart = values["expenseStart"];
+    const dates = values["startDate"];
+    const startDate = dates[0];
+    const endDate = dates[1];
+
+    const q = query(
+      collection(db, "users", currentUser.uid, "expenses"),
+      where("date", ">=", startDate?.toISOString()),
+      where("date", "<=", endDate?.toISOString()),
+      // where("expense", ">=",expenseStart),
+      // where("expense","<=",expenseEnd),
+      // where("category" ,"==",category),
+      // orderBy("expense","desc"),
+      // orderBy("category","desc")
+     
+     
+    );
+setResult([])
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      let total = 0;
+      const exp =[]
+      console.log("snapshot ==> ",snapshot)
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+         console.log("snap shot data => ",data)
+         exp.push({id:doc.id,...data})
+      });
+        exp.map(item=>{
+               if(item?.expense >= expenseStart && item?.expense <= expenseEnd && item?.category == category){
+                     setResult([...result,item])
+               }
+               
+        })
+       
+    });
   };
+
+  console.log("result => ",result)
+
+
 
   return (
-    <main className='container mx-auto '>
-    <div className='flex justify-between max-w-[1542px] mx-auto p-1'>
-      <div className='w-[40%] h-screen mr-10'>
-        {/* Left side */}
-        <div className='flex flex-col gap-6 my-4'>
-          {/* FILTERS */}
-          <Form
-            layout='vertical'
-            size='large'
-            name='basic'
-            wrapperCol={{
-              span: 50,
-            }}
-            style={{
-              paddingTop: '20px',
-              maxWidth: 800,
-            }}
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={handleSubmit}
-            onFinishFailed={() => ''}
-            autoComplete='off'
-          >
-            <Form.Item
-              style={{
-                maxWidth: '100%',
-              }}
-              name='startDate'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your start date!',
-                },
-              ]}
-            >
-              <RangePicker />
-            </Form.Item>
-
-            <div className='flex justify-between'>
-              <Form.Item
-                label='From Expense'
-                style={{
-                  maxWidth: '100%',
-                }}
-                name='expenseStart'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input expense!',
-                  },
-                ]}
-              >
-                <InputNumber placeholder='0' style={{ width: '100%' }} />
-              </Form.Item>
-
-              <Form.Item
-                label='To Expense'
-                name='expenseEnd'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your end date!',
-                  },
-                ]}
-              >
-                <InputNumber placeholder='100000' style={{ width: '100%' }} />
-              </Form.Item>
-            </div>
-
-            <Form.Item
-              name='category'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input category!',
-                },
-              ]}
-            >
-              <Select placeholder='Categories' options={categories} />
-            </Form.Item>
-
-            <Form.Item
+    <main className="container mx-auto ">
+      <div className="flex justify-between max-w-[1542px] mx-auto p-1">
+        <div className="w-[40%] h-screen mr-10">
+          {/* Left side */}
+          <div className="flex flex-col gap-6 my-4">
+            {/* FILTERS */}
+            <Form
+              layout="vertical"
+              size="large"
+              name="basic"
               wrapperCol={{
                 span: 50,
               }}
-              style={{ width: '100%' }}
+              style={{
+                paddingTop: "20px",
+                maxWidth: 800,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={handleSubmit}
+              onFinishFailed={() => ""}
+              autoComplete="off"
             >
-              <Button
-                loading={loading}
-                type='primary'
-                className='w-[100%]'
-                htmlType='submit'
+              <Form.Item
+                style={{
+                  maxWidth: "100%",
+                }}
+                name="startDate"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your start date!",
+                  },
+                ]}
               >
-                Filter
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </div>
-      <div className='w-[60%]'>
+                <RangePicker onChange={(e) => onDateChange(e)} />
+              </Form.Item>
 
-        <div className='my-8'>
-          <DataTable />
+              <div className="flex justify-between">
+                <Form.Item
+                  label="From Expense"
+                  style={{
+                    maxWidth: "100%",
+                  }}
+                  name="expenseStart"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input expense!",
+                    },
+                  ]}
+                >
+                  <InputNumber
+                    placeholder="0"
+                    style={{ width: "100%" }}
+                    onChange={(e) => handleChange(e, "from")}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  label="To Expense"
+                  name="expenseEnd"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your end date!",
+                    },
+                  ]}
+                >
+                  <InputNumber
+                    placeholder="100000"
+                    style={{ width: "100%" }}
+                    onChange={(e) => handleChange(e, "to")}
+                  />
+                </Form.Item>
+              </div>
+
+              <Form.Item
+                name="category"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input category!",
+                  },
+                ]}
+              >
+                <Select
+                  placeholder="Categories"
+                  options={categories}
+                  onChange={(e) => handleChange(e, "category")}
+                />
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  span: 50,
+                }}
+                style={{ width: "100%" }}
+              >
+                <Button
+                  loading={loading}
+                  type="primary"
+                  className="w-[100%]"
+                  htmlType="submit"
+                >
+                  Filter
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+        <div className="w-[60%]">
+          <div className="my-8">
+            <DataTable expenses={result} />
+          </div>
         </div>
       </div>
-    </div>
     </main>
   );
 };
